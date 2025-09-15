34ff80ab7372789b3cbe037280b52b651f9e8ca2
Xavier Lye <ohjokingxd@gmail.com>
2025-09-15 16:29:23 +0700

    api test confirmation

--- src/app/api/registerToken/route.js
+++ src/app/api/registerToken/route.js
@@ -29,3 +29,7 @@ export async function POST(req) {
         return new Response(JSON.stringify({ error: err.message }), { status: 500 });
     }
 }
+
+export async function GET(req) {
+  return new Response(JSON.stringify({ message: "API is alive!" }), { status: 200 });
+}
